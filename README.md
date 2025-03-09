9fec34833cacf31cc83d402a1db8b611626a0e07
Paul <paularelt@gmail.com>
2025-03-09 10:16:59 -0400

    reorganize dockerfiles

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ SDL Circle rendering.
 ## Building
 Building is currently done with [Docker](https://docs.docker.com/get-started/), [vcpkg](https://github.com/microsoft/vcpkg), and BASH.
 
-Run `./build.sh` to kick off the build.
+Run `./build.sh` (Linux) to kick off the build.
 After the building is complete, a local directory will be created with the output.
 You should be able to see a `main` executable in a new `build/` directory in the project directory if the build were successful.
 
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t vcpkg:2024.10.21 -f ./Dockerfile.vcpkg --network host .
-docker build -t sdl_circle:deps -f ./Dockerfile.deps --network host .
-docker build -t sdl_circle --network host --no-cache .
+docker build -t vcpkg:2024.10.21 -f ./docker/Dockerfile.vcpkg --network host .
+docker build -t sdl_circle:deps -f ./docker/Dockerfile.deps --network host .
+docker build -t sdl_circle -f ./docker/Dockerfile --network host --no-cache .
 
 cont_id=$(docker create sdl_circle)
 docker cp $cont_id:/app/build .
